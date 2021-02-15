1fcbb661222512e460c7cca452d4f27f4ac174fc
Manish Kushare <manishkushare9@gmail.com>
2021-02-15 12:48:52 +0530

    fix score update issue

--- block-BJaaen/code/script.js
+++ block-BJaaen/code/script.js
@@ -1,5 +1,5 @@
 function rockPaperScissor ( ){
-        // animation
+    
     let welcome = document.querySelector(".welcome");
     welcome.innerHTML = `<h2>Do you wanna play <span>Rock Scissor and Paper</span></h2>`;
     welcome.classList.add("bounceInDown");
@@ -21,29 +21,36 @@ function rockPaperScissor ( ){
       welcome.style.visibility = "hidden";
       document.querySelector(".box-container").style.visibility = "visible";
     });
+    // document.body.classList.remove("confetti");
 
 
 
     // hero section
-    let userWeapon = document.querySelector(".user-weapons");
     let alliTags = document.querySelectorAll("i");
-    let userScore = 0; //user score
-    let computerScore = 0; //computer score
+    alliTags.forEach(e => {
+        e.style.borderRadius = "10px"
+        e.style.padding = "2rem";
+        e.style.background = "#228EFA";
+
+    })
+    let userScore = 0;
+    let computerScore = 0;
+    let userWeapon = document.querySelector(".user-weapons");
     let userResult = document.querySelector(".user-info").lastElementChild; // h4 tag to update score of user
     let computerResult = document.querySelector(".computer-info").lastElementChild; // h4 tag to update score of computer
-    userResult.innerText = userScore;
-    computerResult.innerText = computerScore;
     userWeapon.addEventListener("click", function(){
-        handleOptions(event,userScore,computerScore,userResult,computerResult);
+        handleOptions(event,userResult,computerResult,alliTags);
 
     })
 
     // restart 
     let restart = document.querySelector(".refresh");
-    restart.addEventListener("click", handleRestart);
+    restart.addEventListener("click", function(){
+        handleRestart(event,alliTags,userResult,computerResult);
+    });
 
     //handle options
-    function handleOptions(event,userScore,computerScore,userResult,computerResult){
+    function handleOptions(event,userResult,computerResult,alliTags){
         resetSelected(alliTags); // first remove "selected" class from all i tags upon selecting option
         event.target.classList.add("selected"); // add class "selected" to the option which is selcted.
         let selectedOption = event.target.getAttribute("id"); // getting id attribute of user selected weapon
@@ -52,16 +59,18 @@ function rockPaperScissor ( ){
         computerSelectedOption.classList.add("selected");// adding class "selected" to randomly selcted computer weapon
         let computerOption = computerSelectedOption.getAttribute("id"); // getting id attribute of computer selected weapon (randomly)
         let display = document.querySelector(".display"); // h2 tag to display the message depanding upon decider result
-        decider(selectedOption,computerOption,display,userResult,computerResult,userScore,computerScore);  // calling decider function to decide the result of round
+        decider(selectedOption,computerOption,display,userResult,computerResult);  // calling decider function to decide the result of round
     }
+    
 
     //round decider function
-    function decider(user,computer,display,userResult,computerResult,userScore = 0,computerScore = 0) {
+    function decider(user,computer,display,userResult,computerResult) {
         if((user === "scissor" && computer === "paper") || (user === "paper" && computer === "rock") || (user === "rock" && computer === "scissor")){
-            userWon(display,userResult,userScore);
+            userWon(display,userResult);
+
         }
         else if((user === "paper" && computer === "scissor")|| (user === "rock" && computer === "paper") || (user === "scissor" && computer === "rock")){
-            userLost(display,computerResult,computerScore)
+            userLost(display,computerResult)
         }
         else {
             mathtie(display);
@@ -70,43 +79,54 @@ function rockPaperScissor ( ){
     //update content on winning
     function userWon(display,userResult,userScore){
         display.innerText = "Congratulations, You Won !";
-        let result = userScoreIncrement(userScore);
-        userResult.innerText = `${result}`;
+        userResult.innerText = userScoreIncrement();
+
     }
     // update content on loosing
-    function userLost(display,computerResult,computerScore){
+    function userLost(display,computerResult){
         display.innerText = "We are sorry, You Lost ! How about trying one more time."
-        let result = computerScoreIncrement(computerScore);
-        computerResult.innerText = `${result}`;
+       computerResult.innerText = computerScoreIncrement();
     }
     // update content on match tie
     function mathtie(display){
         display.innerText = "It's Tie , close game, give it a one more shot !";
     }
+
     // update user score
-    function userScoreIncrement(userScore){
-        return  ++userScore; 
+    function userScoreIncrement(){
+        userScore++;
+        return userScore;
     }
     // update computer score
-    function computerScoreIncrement(computerScore){
-        return  ++computerScore;
+    function computerScoreIncrement(){
+        computerScore++;
+        return computerScore;
     }
     // reset "selected" tag
     function resetSelected(alliTags){
-        alliTags.forEach(element => {
-            element.classList.remove("selected");
+       return  alliTags.forEach(element => {
+            
+             element.classList.remove("selected");
         });
 
     }
 
     //reset the game
-    function handleRestart(event){
-            userResult.innerText = "0";
-            computerResult.innerText =  "0";
+    function handleRestart(){
+            resetScore();
             document.querySelector(".display").innerText = "Let's Play Again !"
             resetSelected(alliTags);
         
     }
+    // reset score
+    function resetScore(){
+        userScore = 0;
+        computerScore = 0;
+        userResult.innerText = userScore;
+        computerResult.innerText = computerScore;
+
+    }
+
 
 }
 
