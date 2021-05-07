b8e445c5db6ed917af770b0f0cfa626445c9dac0
Manish Kushare <=>
2021-05-07 18:07:39 +0530

    update

--- block-BJaaet/code/script.js
+++ block-BJaaet/code/script.js
@@ -1,52 +1,57 @@
-let form = document.querySelector("form");
-let ul = document.querySelector("ul");
-function handleForm(event){
-    event.preventDefault();
-    console.log("test");
-    let movie = {};
-    console.dir(event.target);
-    movie.movieName = form.elements.movie_name.value;
-    movie.isWatched = false;
-    movie.isDelete = "X";
-    createUI(movie);
+function moviePlayList(){
 
-}
-
-function createUI(movie){
-    let li = document.createElement("li");
-    // li.addEventListener("click",function(event){
-    //     handleOperation(event,movie)
-    // })
-    let checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox");
-    checkbox.addEventListener("click",function(event){
-        handleCheckbox(event,movie)
-    });
-    console.dir(checkbox);
-    console.log("is movie watched",movie.isWatched)
-    let span = document.createElement("span");
-    let p = document.createElement("p");
-    p.innerText = movie.movieName;
-    span.innerText = movie.isDelete;
-    span.addEventListener("click",function(event){
-        handleDelete(event,movie)
-    });
-    checkbox.checked = movie.isWatched;
-    li.append(checkbox,p,span);
-    ul.append(li);   
-}
+    let form = document.querySelector("form");
+    let root = document.querySelector("ul");
+    let movieList = [];
 
 
-function handleCheckbox(event,movie){
-    console.log("inside chekbox handle",movie);
-    if(event.target.checked === true){
-        movie.isWatched = true;
+    function handleForm(event){
+        event.preventDefault();
+        movieList.push({
+            name: form.elements.movie_name.value,
+            watched : false,
+        });
+        
+        createUI(event);
+        form.elements.movie_name.value = "";
     }
 
+    function handleDelete(event){
+        let index = event.target.dataset.id;
+        movieList.splice(index,1);
+        createUI();
+    }
 
+    function handleCheckbox(event){
+        let id = event.target.id;
+        movieList[id].watched = !movieList[id].watched;
+        createUI();
+    }
+
+    function createUI(event){
+        root.innerHTML = "";
+        movieList.forEach((movie,index) => {
+            let li = document.createElement("li");
+            let p = document.createElement("p");
+            let span = document.createElement("span");
+            let input = document.createElement("input");
+            input.setAttribute("type","checkbox");
+            input.checked = movie.watched;
+            input.addEventListener("change",handleCheckbox);
+            input.setAttribute("id",index);
+            p.innerText = movie.name;
+            span.innerText = "x";
+            span.setAttribute("data-id",index);
+            span.addEventListener("click",handleDelete);
+            li.append(input,p,span);
+            root.append(li);
+        })
+    }
+    createUI()
+
+
+    form.addEventListener("submit",handleForm);
 
 }
 
-
-
-form.addEventListener("submit", handleForm)
+moviePlayList();
